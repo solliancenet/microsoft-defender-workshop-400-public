213212f6516039084d13bec85882ed65a2c4895c
Chris Givens <givenscj@Hotmail.com>
2021-05-30 14:39:36 -0700

    Update post-install-script01.ps1

--- artifacts/environment-setup/spektra/post-install-script01.ps1
+++ artifacts/environment-setup/spektra/post-install-script01.ps1
@@ -154,6 +154,17 @@ $cred = new-object -typename System.Management.Automation.PSCredential -argument
 
 Connect-AzAccount -Credential $cred | Out-Null
 
+#download the solliance pacakage
+$WebClient = New-Object System.Net.WebClient;
+$WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/common-workshop/main/scripts/common.ps1","C:\LabFiles\common.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/solliancenet/common-workshop/main/scripts/httphelper.ps1","C:\LabFiles\httphelper.ps1")
+
+#run the solliance package
+. C:\LabFiles\Common.ps1
+. C:\LabFiles\HttpHelper.ps1
+
+Set-Executionpolicy unrestricted -force
+
 #download the git repo...
 Write-Host "Download Git repo." -ForegroundColor Green -Verbose
 
